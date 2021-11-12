2b8b981a0ce711258e7132ddff6ccbb3ad077ba3
stephen <sjwelsh2@veriozn.net>
2021-11-12 12:50:46 -0500

    working on the models.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,18 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from config import Config
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
 
 
 if not app.debug:
