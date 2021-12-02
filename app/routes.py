1e8f7c0bf9b1f0bc1e330c5aad615327a2d1fa12
Stephen <stevoschool@gmail.com>
2021-12-02 17:38:11 -0500

    added login function

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import SearchForm
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
+from app.forms import SearchForm, LoginForm, RegistrationForm
 from app.isbndb_request import ISBNDB
 from app.models import User, Listings, Books
 
@@ -9,19 +11,48 @@ def index():
     return render_template('base.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    return render_template('base.html')
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route('/browse')
@@ -30,6 +61,7 @@ def browse():
 
 
 @app.route('/post')
+@login_required
 def post():
     return render_template('post.html')
 
