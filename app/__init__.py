7199eced24311a23934602441937e535a15e0f64
stephen <sjwelsh2@veriozn.net>
2021-12-11 13:36:29 -0500

    added caching, removal of posts

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_caching import Cache
 from config import Config
 
 app = Flask(__name__)
+config = {
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+f_cache = Cache()
+f_cache.init_app(app, config=config)
 app.debug = True
 bootstrap = Bootstrap(app)
 app.config.from_object(Config)
